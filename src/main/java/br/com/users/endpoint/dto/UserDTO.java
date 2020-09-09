b0b4445782d544fd06e2a685861c4597cf5ff72b
Eric <castro.eric@hotmail.com>
2020-09-08 22:01:13 -0300

    first commit

---
+++ src/main/java/br/com/users/endpoint/dto/UserDTO.java
@@ -0,0 +1,35 @@
+package br.com.users.endpoint.dto;
+
+import br.com.users.endpoint.entity.User;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class UserDTO {
+
+    private String racf;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private LocalDateTime dateBirth;
+    private String genre;
+    private String phoneNumber;
+
+    public UserDTO() {
+    }
+
+    public UserDTO(User user) {
+        this.racf = user.getRacf();
+        this.firstName = user.getFirstName();
+        this.lastName = user.getLastName();
+        this.email = user.getEmail();
+        this.password = user.getPassword();
+        this.dateBirth = user.getDateBirth();
+        this.genre = user.getGenre();
+        this.phoneNumber = user.getPhoneNumber();
+    }
+}
